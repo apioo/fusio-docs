bf68460c51e0d1fdea0c658ab0bfa0a6c8134dc1
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-11-06 03:07:16 +0100

    use trailing slash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,6 +13,7 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/fusio_32px.png',
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
